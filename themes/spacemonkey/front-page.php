6933ade2d4f1d898cfcf1d362d55e168f17a6285
Dave Kellam <dave@eightface.com>
2018-03-06 00:13:06 -0500

    Hide wpstats image

--- themes/spacemonkey/front-page.php
+++ themes/spacemonkey/front-page.php
@@ -16,6 +16,7 @@
 			li { display: inline; list-style: none; }
 			li:not(:last-child):after { content: " / "; color: grey; padding: .5em; }
 			hr { border: 0; height: 0; border-top: 1px solid rgba(0, 0, 0, 0.1); border-bottom: 1px solid rgba(255, 255, 255, 0.3); }
+			img#wpstats { display: none; }
 		</style>
 	</head>
 	<body>
