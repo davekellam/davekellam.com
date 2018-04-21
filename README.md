d4b4af5304548bb27413bf016ec1ed7323f1da13
Dave Kellam <dave@eightface.com>
2018-04-21 15:56:12 -0400

    Remove titles, initial meta rework

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 davekellam.com
 ==============
 
-Repo for davekellam.com
+The theme and plugins that run the site. The repo is setup to facilitate local development in Chassis.io
+
+
--- themes/spacemonkey/inc/template-tags.php
+++ themes/spacemonkey/inc/template-tags.php
@@ -18,15 +18,15 @@ if ( ! function_exists( '_s_posted_on' ) ) :
 		}
 
 		$time_string = sprintf( $time_string,
-			esc_attr( get_the_date( 'c' ) ),
-			esc_html( get_the_date() ),
-			esc_attr( get_the_modified_date( 'c' ) ),
-			esc_html( get_the_modified_date() )
+			esc_attr( get_the_date( 'Y/m/d \a\t g:ia' ) ),
+			esc_html( get_the_date( 'Y/m/d \a\t g:ia' ) ),
+			esc_attr( get_the_modified_date( 'Y/m/d \a\t g:ia' ) ),
+			esc_html( get_the_modified_date( 'Y/m/d \a\t g:ia' ) )
 		);
 
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
-			esc_html_x( 'Posted on %s', 'post date', '_s' ),
+			esc_html_x( '%s', 'post date', '_s' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 
@@ -59,55 +59,19 @@ if ( ! function_exists( '_s_entry_footer' ) ) :
 		// Hide category and tag text for pages.
 		if ( 'post' === get_post_type() ) {
 			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', '_s' ) );
-			if ( $categories_list ) {
+			//$categories_list = get_the_category_list( esc_html__( ', ', '_s' ) );
+			//if ( $categories_list ) {
 				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', '_s' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-			}
+			//	printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', '_s' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+			//}
 
 			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', '_s' ) );
+			$tags_list = get_the_tag_list( '', esc_html_x( ' #', 'list item separator', '_s' ) );
 			if ( $tags_list ) {
 				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', '_s' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+				printf( '<span class="tags-links">' . esc_html__( '', '_s' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 			}
 		}
-
-		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-			echo '<span class="comments-link">';
-			comments_popup_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: post title */
-						__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', '_s' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				)
-			);
-			echo '</span>';
-		}
-
-		edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Edit <span class="screen-reader-text">%s</span>', '_s' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			),
-			'<span class="edit-link">',
-			'</span>'
-		);
 	}
 endif;
 
