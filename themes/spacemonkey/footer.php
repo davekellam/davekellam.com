e4fa9b11a89b56c36ebfc358778104d164cc3e4b
Dave Kellam <dave@eightface.com>
2018-04-21 01:22:13 -0400

    Add elsewhere links to footer

--- themes/spacemonkey/footer.php
+++ themes/spacemonkey/footer.php
@@ -13,6 +13,21 @@
 		<div class="site-info">
 			
 		</div><!-- .site-info -->
+
+		<div class="elsewhere">
+			<h3>Elsewhere</h3>
+			<ul>
+				<li><a href="https://www.eightface.com">eightface.com</a></li>
+				<li><a href="http://helveti.ca">helveti.ca</a></li>
+				<li><a href="https://twitter.com/davekellam/">twitter</a></li>
+				<li><a href="https://pinboard.in/u:davekellam">pinboard</a></li>
+				<li><a href="https://www.flickr.com/photos/davekellam/">flickr</a></li>
+				<li><a href="https://instagram.com/davekellam">instagram</a></li>
+				<li><a href="https://last.fm/user/eightface/">last.fm</a></li>
+				<li><a href="https://github.com/davekellam/">github</a></li>
+				<li><a href="https://davekellam.tumblr.com">tumblr</a></li>
+			</ul>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
