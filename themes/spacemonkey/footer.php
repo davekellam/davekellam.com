539efc4fdb0baf40a8521bdece35abcd265aeaf1
Dave Kellam <dave@eightface.com>
2018-04-25 16:57:32 -0400

    Remove elsewhere links from footer
    Moved to about page

--- themes/spacemonkey/footer.php
+++ themes/spacemonkey/footer.php
@@ -11,23 +11,8 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			
+		<p>&copy;<?php echo date( 'Y' ); ?> &middot; <a href="http://davekellam.com">Dave Kellam</a></p>
 		</div><!-- .site-info -->
-
-		<div class="elsewhere">
-			<h3>Elsewhere</h3>
-			<ul>
-				<li><a href="https://www.eightface.com">eightface.com</a></li>
-				<li><a href="http://helveti.ca">helveti.ca</a></li>
-				<li><a href="https://twitter.com/davekellam/">twitter</a></li>
-				<li><a href="https://pinboard.in/u:davekellam">pinboard</a></li>
-				<li><a href="https://www.flickr.com/photos/davekellam/">flickr</a></li>
-				<li><a href="https://instagram.com/davekellam">instagram</a></li>
-				<li><a href="https://last.fm/user/eightface/">last.fm</a></li>
-				<li><a href="https://github.com/davekellam/">github</a></li>
-				<li><a href="https://davekellam.tumblr.com">tumblr</a></li>
-			</ul>
-		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
