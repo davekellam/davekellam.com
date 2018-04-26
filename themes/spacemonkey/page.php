c78e45a93fae29e6d520039700342c53b05ac6b5
Dave Kellam <dave@eightface.com>
2018-04-26 12:21:16 -0400

    Cleaning up default page template
    Bringing the partial in directly, removing comments and edit links

--- themes/spacemonkey/page.php
+++ themes/spacemonkey/page.php
@@ -1,15 +1,6 @@
 <?php
 /**
  * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package _s
  */
 
 get_header();
@@ -18,19 +9,21 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+		<?php while ( have_posts() ) : the_post(); ?>
 
-			get_template_part( 'template-parts/content', 'page' );
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+				</header>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				<?php _s_post_thumbnail(); ?>
 
-		endwhile; // End of the loop.
-		?>
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+			</article>
+
+		<?php endwhile; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
