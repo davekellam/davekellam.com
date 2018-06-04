8866c957d8f798b443ae02ce30dc2ac67cec249d
Dave Kellam <dave@eightface.com>
2018-06-04 15:50:49 -0400

    Add some pagination styling

--- themes/spacemonkey/index.php
+++ themes/spacemonkey/index.php
@@ -15,9 +15,9 @@
 get_header();
 
 $pagination = get_the_posts_pagination( array(
-	'mid_size' => 3,
-	'prev_text' => __( '&laquo;', 'spacemonkey' ),
-	'next_text' => __( '&raquo;', 'spacemonkey' ),
+	'mid_size' => 5,
+	'prev_text' => __( '&larr;', 'spacemonkey' ),
+	'next_text' => __( '&rarr;', 'spacemonkey' ),
 ) );
 
 ?>
