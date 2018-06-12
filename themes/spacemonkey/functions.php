d11012df39fc1191d9573423ef7366a390e6fa41
Dave Kellam <dave@eightface.com>
2018-06-12 10:52:17 -0400

    Set content width to 1200
    Think this is affecting images uploaded from mobile, so they’re not getting the right large size. Probably need to sort out image dimension in general.

--- themes/spacemonkey/functions.php
+++ themes/spacemonkey/functions.php
@@ -59,7 +59,7 @@ if ( ! function_exists( '_s_setup' ) ) :
 			'gallery',
 			'caption',
 		) );
-
+		
 		remove_action( 'wp_head', 'wlwmanifest_link' );
 	}
 endif;
@@ -76,7 +76,7 @@ function _s_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( '_s_content_width', 640 );
+	$GLOBALS['content_width'] = apply_filters( '_s_content_width', 1200 );
 }
 add_action( 'after_setup_theme', '_s_content_width', 0 );
 
