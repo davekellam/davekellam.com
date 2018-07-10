b40a0e4d385bacd1b73423762797c53a5bed4f75
Dave Kellam <dave@eightface.com>
2018-07-10 15:25:49 -0400

    Adding now page template
    Mostly just a duplicate of the page.php template for now

---
+++ themes/spacemonkey/page-now.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template: Now
+ *
+ * The template for /now/
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+				</header>
+
+				<?php _s_post_thumbnail(); ?>
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+			</article>
+
+		<?php endwhile; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
