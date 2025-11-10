13338e16bc75650a4af48852a13a18a168e80baa
Dave Kellam <kellam@gmail.com>
2025-11-10 18:16:53 -0500

    Add phpcs ruleset and linting tasks

--- themes/davekellam-25/index.php
+++ themes/davekellam-25/index.php
@@ -1,9 +1,15 @@
 <?php
+/**
+ * This is the main template file.
+ *
+ * @package DaveKellam-25
+ */
+
 get_header();
 
 while ( have_posts() ) :
-    the_post();
-    the_content();
+	the_post();
+	the_content();
 endwhile;
 
 get_footer();
