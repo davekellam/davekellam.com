e0320d0fd190da83c328f3a222bf63ffa32a74a2
Dave Kellam <kellam@gmail.com>
2025-11-12 20:00:30 -0500

    Add album post type

--- mu-plugins/dk-core/post-types.php
+++ mu-plugins/dk-core/post-types.php
@@ -8,7 +8,7 @@
 namespace DaveKellam\Core\PostTypes;
 
 // Not alphabetical, defines order in left-hand column
-// add_action( 'init', __NAMESPACE__ . '\\albums' );
+add_action( 'init', __NAMESPACE__ . '\\albums' );
 
 /**
  * Register the News post type
@@ -20,7 +20,11 @@ function albums() {
 			'menu_icon' => 'dashicons-album',
 			'supports'  => [ 'title', 'editor', 'thumbnail' ],
 			'public'    => false,
-			'show_ui'   => true
+			'show_ui'   => true,
 		],
+		[
+			'singular' => 'Album',
+			'plural'   => 'Albums',
+		]
 	);
 }
