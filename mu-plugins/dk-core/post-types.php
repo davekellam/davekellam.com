e06c9d54bbebcc5a6d0e95b0edb01cfd12ed8eaf
Dave Kellam <kellam@gmail.com>
2025-11-13 19:43:48 -0500

    Fix extended cpts check
    Was loading way too early 😅

--- mu-plugins/dk-core/post-types.php
+++ mu-plugins/dk-core/post-types.php
@@ -5,17 +5,17 @@
 
 namespace DaveKellam\Core\PostTypes;
 
-// Bail if extended CPTs is not available (i.e. no composer install)
-if ( ! function_exists( 'register_extended_post_type' ) ) {
-	return;
-}
-
 add_action( 'init', __NAMESPACE__ . '\\albums' );
 
 /**
  * Register the News post type
  */
 function albums() {
+	// Bail if extended CPTs is not available (i.e. no composer install)
+	if ( ! function_exists( 'register_extended_post_type' ) ) {
+		return;
+	}
+
 	register_extended_post_type(
 		'record-album',
 		[
