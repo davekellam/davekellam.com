319605be417ea42c6434665d0cfda29a8bc32458
Dave Kellam <kellam@gmail.com>
2025-11-25 13:15:49 -0500

    Bump theme version

--- themes/davekellam-25/functions.php
+++ themes/davekellam-25/functions.php
@@ -4,7 +4,7 @@
  */
 
 // Useful global constants.
-define( 'DAVEKELLAM_VERSION', '25.11.18' );
+define( 'DAVEKELLAM_VERSION', '25.11.25' );
 define( 'DAVEKELLAM_TEMPLATE_URL', get_template_directory_uri() );
 define( 'DAVEKELLAM_PATH', get_template_directory() . '/' );
 define( 'DAVEKELLAM_INC', DAVEKELLAM_PATH . 'includes/' );
