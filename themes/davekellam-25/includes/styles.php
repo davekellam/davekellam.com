720bdc6f0211c02ed3b2a733248f8a146a757fdf
Dave Kellam <kellam@gmail.com>
2025-11-25 13:17:50 -0500

    Remove unused editor and admin styles

--- themes/davekellam-25/includes/styles.php
+++ themes/davekellam-25/includes/styles.php
@@ -10,15 +10,8 @@ namespace DaveKellam\Theme\DK25\Styles;
 // Load Frontend styles
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\load_styles' );
 
-// Load Print styles
-add_action( 'wp_print_styles', __NAMESPACE__ . '\\load_print_styles' );
-
-// Load scripts and styles for admin areas:
-add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\load_admin_styles', 20 );
-add_action( 'admin_init', __NAMESPACE__ . '\\add_editor_styles' );
-
 // Load login styles
-add_action( 'login_enqueue_scripts', __NAMESPACE__ . '\\load_login_styles' );
+// add_action( 'login_enqueue_scripts', __NAMESPACE__ . '\\load_login_styles' );
 
 
 /**
@@ -31,50 +24,9 @@ function load_styles() {
 		'frontend',
 		DAVEKELLAM_TEMPLATE_URL . '/dist/css/frontend.min.css',
 		[],
-		filemtime( DAVEKELLAM_PATH . 'dist/css/frontend.min.css' ),
+		DAVEKELLAM_VERSION,
 		'screen'
 	);
-}
-
-/**
- * Enqueue styles for print
- *
- * @return void
- */
-function load_print_styles() {
-	wp_enqueue_style(
-		'print',
-		DAVEKELLAM_TEMPLATE_URL . '/dist/css/print.min.css',
-		[],
-		filemtime( DAVEKELLAM_PATH . 'dist/css/print.min.css' ),
-		'print'
-	);
-}
-
-
-/**
- * Register and load admin CSS
- */
-function load_admin_styles() {
-	// Register Styles
-	wp_register_style(
-		'admin-style',
-		get_template_directory_uri() . '/dist/css/admin.min.css',
-		false,
-		DAVEKELLAM_VERSION
-	);
-
-	// Enqueue Styles
-	wp_enqueue_style( 'admin-style' );
-}
-
-/**
- * Add a stylesheet to customize the appearance of the editor
- */
-function add_editor_styles() {
-	$stylesheet = './dist/css/editor.min.css';
-
-	add_editor_style( $stylesheet );
 }
 
 /**
@@ -85,6 +37,6 @@ function load_login_styles() {
 		'login-style',
 		get_stylesheet_directory_uri() . '/dist/css/login.min.css',
 		false,
-		filemtime( DAVEKELLAM_PATH . 'dist/css/login.min.css' )
+		DAVEKELLAM_VERSION
 	);
 }
