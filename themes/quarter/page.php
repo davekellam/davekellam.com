0fb87e803cf7bbe44f7070dc63adc04f9df40916
Dave Kellam <kellam@gmail.com>
2026-07-13 23:22:54 -0400

    Remove entry-title class from page titles

--- themes/quarter/page.php
+++ themes/quarter/page.php
@@ -12,7 +12,7 @@ the_post();
 <main class="site-main" id="main">
 	<article id="post-<?php the_ID(); ?>" <?php post_class( 'entry' ); ?>>
 		<header class="entry-header">
-			<h1 class="entry-title"><?php the_title(); ?></h1>
+			<h1><?php the_title(); ?></h1>
 		</header>
 
 		<?php if ( has_post_thumbnail() ) : ?>
